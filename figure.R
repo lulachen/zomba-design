948e59100afa09ce3d1a2bb397943a62ab725970
Equal Access Nigeria <eakdi-2.2@Equals-MacBook-Pro.local>
2017-01-25 21:53:09 +0100

    adding Nuole's figure.

--- figure.R
+++ figure.R
@@ -1,18 +1,18 @@
-setwd("C:/Users/Nuole/Dropbox/metaketa_taxation/Power Analysis Code")
+setwd("C:/Users/Nuole/Dropbox/metaketa_taxation/from_zomba-design")
 
 load("parms.rda")
 View(parms)
 
-parm50<-subset(parms, n==50)
-parm120<-subset(parms, n==120)
-parm220<-subset(parms,n==220)
+parm60<-subset(parms, n==60)
+parm70<-subset(parms, n==70)
+parm80<-subset(parms,n==80)
 
-png("power.png")
-plot(parm50$tau, parm50$pow, type="b", pch=16, col = "red",ylab="Power", 
+png("morepower_cluster.png")
+plot(parm60$tau, parm60$pow, type="b", pch=16, col = "red",ylab="Power", 
      xlab= expression(tau), main= "Figure 1. Power Analysis with Covariance Adjustment", cex.main=.9)
-points(parm120$tau, parm120$pow, type="b", pch = 16, col="blue")
-points(parm220$tau, parm220$pow, type="b", pch = 16, col="gray")
+points(parm70$tau, parm70$pow, type="b", pch = 16, col="blue")
+points(parm80$tau, parm80$pow, type="b", pch = 16, col="gray")
 abline(a=.8, b = 0, col="black", lty=2)
-legend("bottomright", inset=c(.03,.03), c("50", "120", "220"), 
+legend("bottomright", inset=c(.03,.03), c("60", "70", "80"), 
        title="Sample Size", col=c("red", "blue", "gray"), pch=16, lty=1, cex=0.75)
 dev.off()
